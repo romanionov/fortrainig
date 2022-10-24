4a4d544be48d62a45ba12d29f648441e8eaa20e4
Roman Ionov <romales243@gmail.com>
2022-10-24 21:41:26 +0300

    addition print(= * 20)

--- Equation.py
+++ Equation.py
@@ -3,11 +3,13 @@ print("Введите первую точку")
 x1 = float(input('X: '))
 y1 = float(input('Y: '))
 
+print('=' * 20)
 print("\nВведите вторую точку")
 
 x2 = float(input('X: '))
 y2 = float(input('Y: '))
 
+print('=' * 20)
 print("Уравнение прямой, проходящей через эти точки:")
 x_diff = x1 - x2
 y_diff = y1 - y2
